79653c5f2205b7868929af72cbf8ecd9adb3d601
watawo4 <107882109+watabekoutarou@users.noreply.github.com>
2023-09-06 23:06:29 +0900

    Create speech1.py

---
+++ speech1.py
@@ -0,0 +1,17 @@
+import speech_recognition as sr
+
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print("Say something!")
+    audio = r.listen(source)
+
+try:
+    print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
+except sr.UnknownValueError:
+    print("Google Speech Recognition could not understand audio")
+except sr.RequestError as e:
+    print("Could not request results from Google Speech Recognition service; {0}".format(e))
+"""
+if r.recognize_google(audio)=="stop":
+    print("音声認識を終了します")
+"""
