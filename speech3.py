32edd5c77fd1105963f1b74fa0ee668e25db8937
watawo4 <107882109+watabekoutarou@users.noreply.github.com>
2023-09-11 19:15:20 +0900

    Update speech3.py

--- speech3.py
+++ speech3.py
@@ -2,17 +2,17 @@ import speech_recognition as sr
 import sys
 r = sr.Recognizer()
  
-with sr.AudioFile("sampleJp.wav") as source:
+with sr.AudioFile("sampleMiss.wav") as source:
     audio = r.record(source)
  
 #事前に設定した探索する単語群(vector<str>)
 strSet = ["水筒","リモコン","時計"]
 #単語と学習モデルのラベリングのペア
-dict = {"水筒":"bottle","リモコン":"remote control","時計":"watch"}
+dict = {"水筒":"bottle","リモコン":"remote control","時計":"watch","nothing":"nothing"}
+
 text = r.recognize_google(audio,language = 'ja-JP')
 print(text)
-print(text[0:4])
-print(text[4])
+
 flag = False
 cnt = 0
 for i in range(len(text)-3):
@@ -20,15 +20,11 @@ for i in range(len(text)-3):
         flag = True
         locate = i+5
 target = "nothing"
-if flag == False:
+if not flag :
     sys.exit()
 for x in strSet:
     if len(text)-locate>=0 and x==text[locate:locate+len(x)]:
         #print(x)
         target=x
         break
-print(f'target is{target} ,this label is {dict[target]}')
-
-
-
-
+print(f'target is {target} ,this label is {dict[target]}')
